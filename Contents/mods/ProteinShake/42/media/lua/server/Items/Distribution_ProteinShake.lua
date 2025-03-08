d23ffb5f472585b3a9433b049a8600a3fca85759
Satana Charuwichitratana (Mick) <satana.charu@gmail.com>
2025-03-09 01:12:27 +0700

    feat: Procedural distributions

---
+++ Contents/mods/ProteinShake/42/media/lua/server/Items/Distribution_ProteinShake.lua
@@ -0,0 +1,130 @@
+require "Items/ProceduralDistributions"
+
+table.insert(ProceduralDistributions["list"]["CrateCannedFood"].items, "ProteinShake.ProteinPowderGSIsolate");
+table.insert(ProceduralDistributions["list"]["CrateCannedFood"].items, 2);
+table.insert(ProceduralDistributions["list"]["CrateCannedFood"].items, "ProteinShake.ProteinPowderGSWhey");
+table.insert(ProceduralDistributions["list"]["CrateCannedFood"].items, 2);
+table.insert(ProceduralDistributions["list"]["CrateCannedFood"].items, "ProteinShake.ProteinPowderSMass");
+table.insert(ProceduralDistributions["list"]["CrateCannedFood"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, "ProteinShake.ProteinShakerGSIsolate");
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, 2);
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, "ProteinShake.ProteinShakerGSWhey");
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, 2);
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, "ProteinShake.ProteinShakerSMass");
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, 2);
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, "ProteinShake.ProteinPowderGSIsolate");
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, 2);
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, "ProteinShake.ProteinPowderGSWhey");
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, 2);
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, "ProteinShake.ProteinPowderSMass");
+table.insert(ProceduralDistributions["list"]["GasStorageCombo"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["GigamartBottles"].items, "ProteinShake.ProteinShakerGSIsolate");
+table.insert(ProceduralDistributions["list"]["GigamartBottles"].items, 10);
+table.insert(ProceduralDistributions["list"]["GigamartBottles"].items, "ProteinShake.ProteinShakerGSWhey");
+table.insert(ProceduralDistributions["list"]["GigamartBottles"].items, 10);
+table.insert(ProceduralDistributions["list"]["GigamartBottles"].items, "ProteinShake.ProteinShakerSMass");
+table.insert(ProceduralDistributions["list"]["GigamartBottles"].items, 10);
+
+table.insert(ProceduralDistributions["list"]["GigamartCannedFood"].items, "ProteinShake.ProteinPowderGSIsolate");
+table.insert(ProceduralDistributions["list"]["GigamartCannedFood"].items, 4);
+table.insert(ProceduralDistributions["list"]["GigamartCannedFood"].items, "ProteinShake.ProteinPowderGSWhey");
+table.insert(ProceduralDistributions["list"]["GigamartCannedFood"].items, 4);
+table.insert(ProceduralDistributions["list"]["GigamartCannedFood"].items, "ProteinShake.ProteinPowderSMass");
+table.insert(ProceduralDistributions["list"]["GigamartCannedFood"].items, 4);
+
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, "ProteinShake.ProteinShakerGSIsolate");
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, 2);
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, "ProteinShake.ProteinShakerGSWhey");
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, 2);
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, "ProteinShake.ProteinShakerSMass");
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, 2);
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, "ProteinShake.ProteinPowderGSIsolate");
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, 2);
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, "ProteinShake.ProteinPowderGSWhey");
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, 2);
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, "ProteinShake.ProteinPowderSMass");
+table.insert(ProceduralDistributions["list"]["GymLockers"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["PoolLockers"].items, "ProteinShake.ProteinShakerGSIsolate");
+table.insert(ProceduralDistributions["list"]["PoolLockers"].items, 2);
+table.insert(ProceduralDistributions["list"]["PoolLockers"].items, "ProteinShake.ProteinShakerGSWhey");
+table.insert(ProceduralDistributions["list"]["PoolLockers"].items, 2);
+table.insert(ProceduralDistributions["list"]["PoolLockers"].items, "ProteinShake.ProteinShakerSMass");
+table.insert(ProceduralDistributions["list"]["PoolLockers"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["PrisonGuardLockers"].items, "ProteinShake.ProteinShakerGSIsolate");
+table.insert(ProceduralDistributions["list"]["PrisonGuardLockers"].items, 2);
+table.insert(ProceduralDistributions["list"]["PrisonGuardLockers"].items, "ProteinShake.ProteinShakerGSWhey");
+table.insert(ProceduralDistributions["list"]["PrisonGuardLockers"].items, 2);
+table.insert(ProceduralDistributions["list"]["PrisonGuardLockers"].items, "ProteinShake.ProteinShakerSMass");
+table.insert(ProceduralDistributions["list"]["PrisonGuardLockers"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, "ProteinShake.ProteinShakerGSIsolate");
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, 2);
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, "ProteinShake.ProteinShakerGSWhey");
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, 2);
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, "ProteinShake.ProteinShakerSMass");
+table.insert(ProceduralDistributions["list"]["KitchenBottles"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, "ProteinShake.ProteinPowderGSIsolate");
+table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, 2);
+table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, "ProteinShake.ProteinPowderGSWhey");
+table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, 2);
+table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, "ProteinShake.ProteinPowderSMass");
+table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["KitchenDryFood"].items, "ProteinShake.ProteinPowderGSIsolate");
+table.insert(ProceduralDistributions["list"]["KitchenDryFood"].items, 1);
+table.insert(ProceduralDistributions["list"]["KitchenDryFood"].items, "ProteinShake.ProteinPowderGSWhey");
+table.insert(ProceduralDistributions["list"]["KitchenDryFood"].items, 1);
+table.insert(ProceduralDistributions["list"]["KitchenDryFood"].items, "ProteinShake.ProteinPowderSMass");
+table.insert(ProceduralDistributions["list"]["KitchenDryFood"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["KitchenRandom"].items, "ProteinShake.ProteinPowderGSIsolate");
+table.insert(ProceduralDistributions["list"]["KitchenRandom"].items, 1);
+table.insert(ProceduralDistributions["list"]["KitchenRandom"].items, "ProteinShake.ProteinPowderGSWhey");
+table.insert(ProceduralDistributions["list"]["KitchenRandom"].items, 1);
+table.insert(ProceduralDistributions["list"]["KitchenRandom"].items, "ProteinShake.ProteinPowderSMass");
+table.insert(ProceduralDistributions["list"]["KitchenRandom"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["SafehouseFood"].items, "ProteinShake.ProteinShakerGSIsolate");
+table.insert(ProceduralDistributions["list"]["SafehouseFood"].items, 1);
+table.insert(ProceduralDistributions["list"]["SafehouseFood"].items, "ProteinShake.ProteinShakerGSWhey");
+table.insert(ProceduralDistributions["list"]["SafehouseFood"].items, 1);
+table.insert(ProceduralDistributions["list"]["SafehouseFood"].items, "ProteinShake.ProteinShakerSMass");
+table.insert(ProceduralDistributions["list"]["SafehouseFood"].items, 1);
+table.insert(ProceduralDistributions["list"]["SafehouseFood"].items, "ProteinShake.ProteinPowderGSIsolate");
+table.insert(ProceduralDistributions["list"]["SafehouseFood"].items, 1);
+table.insert(ProceduralDistributions["list"]["SafehouseFood"].items, "ProteinShake.ProteinPowderGSWhey");
+table.insert(ProceduralDistributions["list"]["SafehouseFood"].items, 1);
+table.insert(ProceduralDistributions["list"]["SafehouseFood"].items, "ProteinShake.ProteinPowderSMass");
+table.insert(ProceduralDistributions["list"]["SafehouseFood"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["SafehouseFood_Mid"].items, "ProteinShake.ProteinShakerGSIsolate");
+table.insert(ProceduralDistributions["list"]["SafehouseFood_Mid"].items, 1);
+table.insert(ProceduralDistributions["list"]["SafehouseFood_Mid"].items, "ProteinShake.ProteinShakerGSWhey");
+table.insert(ProceduralDistributions["list"]["SafehouseFood_Mid"].items, 1);
+table.insert(ProceduralDistributions["list"]["SafehouseFood_Mid"].items, "ProteinShake.ProteinShakerSMass");
+table.insert(ProceduralDistributions["list"]["SafehouseFood_Mid"].items, 1);
+table.insert(ProceduralDistributions["list"]["SafehouseFood_Mid"].items, "ProteinShake.ProteinPowderGSIsolate");
+table.insert(ProceduralDistributions["list"]["SafehouseFood_Mid"].items, 1);
+table.insert(ProceduralDistributions["list"]["SafehouseFood_Mid"].items, "ProteinShake.ProteinPowderGSWhey");
+table.insert(ProceduralDistributions["list"]["SafehouseFood_Mid"].items, 1);
+table.insert(ProceduralDistributions["list"]["SafehouseFood_Mid"].items, "ProteinShake.ProteinPowderSMass");
+table.insert(ProceduralDistributions["list"]["SafehouseFood_Mid"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["SchoolLockers"].items, "ProteinShake.ProteinShakerGSIsolate");
+table.insert(ProceduralDistributions["list"]["SchoolLockers"].items, 1);
+table.insert(ProceduralDistributions["list"]["SchoolLockers"].items, "ProteinShake.ProteinShakerGSWhey");
+table.insert(ProceduralDistributions["list"]["SchoolLockers"].items, 1);
+table.insert(ProceduralDistributions["list"]["SchoolLockers"].items, "ProteinShake.ProteinShakerSMass");
+table.insert(ProceduralDistributions["list"]["SchoolLockers"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["SportStoreAccessories"].items, "ProteinShake.ProteinShakerGSIsolate");
+table.insert(ProceduralDistributions["list"]["SportStoreAccessories"].items, 1);
+table.insert(ProceduralDistributions["list"]["SportStoreAccessories"].items, "ProteinShake.ProteinShakerGSWhey");
+table.insert(ProceduralDistributions["list"]["SportStoreAccessories"].items, 1);
+table.insert(ProceduralDistributions["list"]["SportStoreAccessories"].items, "ProteinShake.ProteinShakerSMass");
+table.insert(ProceduralDistributions["list"]["SportStoreAccessories"].items, 1);
